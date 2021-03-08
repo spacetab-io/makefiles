90bee60dcebec80e01175552f44f4c803bc2d26d
Sergey Gladkovskiy <SMGladkovskiy@gmail.com>
2021-03-08 12:19:27 +0300

    Update tests.mk

--- golang/tests.mk
+++ golang/tests.mk
@@ -1,10 +1,14 @@
-.PHONY: go_tests go_unit_tests go_coverage_html
+.PHONY: go_tests go_tests_html go_run_unit_tests go_get_html_coverage
 
-go_unit_tests:
-	@go test -cover -race -count=1 -timeout 1s -coverprofile=c.out -v ./... && go tool cover -func=c.out
+go_run_unit_tests:
+	@go test -cover -race -timeout 1s -coverprofile=c.out -v ./...
+	@go tool cover -func=c.out
 
-go_coverage_html:
+go_get_html_coverage:
 	@go tool cover -html=c.out -o coverage.html
-	rm c.out
 
-go_tests: go_unit_tests go_coverage_html
+rm_cover_profile:
+	@rm c.out
+
+go_tests: go_run_unit_tests rm_cover_profile
+go_tests_html: go_run_unit_tests go_get_html_coverage rm_cover_profile
